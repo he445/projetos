8bd448bb86f5ca938c95452663609c57ccc50ca0
he445 <103337053+he445@users.noreply.github.com>
2022-07-04 20:37:00 -0300

    0.7

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-window.addEventListener("scroll", function() {showFunction()});
-
-function showFunction() {
-    if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
-        document.getElementsByClassName("showdownon").style.display = "none";
-    } else {
-        document.getElementById("showdownon").style.display = "block";
-    }
+window.onload=function(){
+  
+let projetoli1 = document.querySelector("#pojetoli1")
+let projetoli2 = document.querySelector("#pojetoli2")
+let projetoli3 = document.querySelector("#pojetoli3")
+let pojeto = document.querySelector("#Projetos");
+let contato = document.querySelector("#contato");
+let minhasskils = document.querySelector("#minhasskils");
+projetoli1.addEventListener("click",function () {
+  pojeto.style.display = "block";
+  console.log("ok");
+});
+projetoli2.addEventListener("click",function () {
+    contato.style.display = "block";
+    console.log("ok");
+  });
+  projetoli3.addEventListener("click",function () {
+    minhasskils.style.display = "block";
+    console.log("ok");
+  });
 }
-showFunction()
